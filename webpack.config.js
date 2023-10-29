25fbd1fb6ed4c88cee57bf3945649c0966de26d6
thadeus <thadeusnyariki94@gmail.com>
2023-10-29 17:51:41 +0300

    made changes to webpack.config.js and package.json to get up and running

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,21 @@
 module.exports = {
-	entry: './src/app.js',
-	output: {
-		path: './dist',
-		filename:'app.bundle.js'
-	},
-	module: {
-		loaders: [{
-			exclude: '/node_modules/',
-			loader: 'babel-loader',
-			query: {
-				presets: ['es2015']
-			}
-		}]
-	}
-}
+  entry: __dirname + '/src/app.js',
+  output: {
+    path: __dirname + '/dist',
+    filename: 'app.bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/, // Apply the loader only to JavaScript files
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'], // Use @babel/preset-env for compiling JavaScript
+          },
+        },
+      },
+    ],
+  },
+};
