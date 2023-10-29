6ff1f03fbfbb5ba8884023b77b3e8a51229095d2
thadeus <thadeusnyariki94@gmail.com>
2023-10-30 00:54:50 +0300

    observables from scratch

--- src/app.js
+++ src/app.js
@@ -1,4 +1,38 @@
 import $ from 'jquery';
 import Rx from 'rxjs/Rx';
 
-console.log('RxJS Boiler Running...');
+const source$ = new Rx.Observable(
+    observer => {
+        console.log('creating observable');
+
+        // to emit a value
+        observer.next('Hello World');
+        observer.next('Another Value');
+
+        // lets do error
+        observer.error(new Error('Error: something went wrong'));
+
+        // lets do a timeout
+        setTimeout(() => {
+            observer.next('Yet another value');
+
+            // incase we want to receive completed message
+            observer.complete();
+        }, 3000);
+    }
+);
+
+source$
+// making error user/more friendly
+.catch(err => Rx.Observable.of(err))
+.subscribe(
+    x => {
+        console.log(x);
+    },
+    err => {
+        console.log(err);
+    },
+    complete => {
+        console.log('completed');
+    }
+)
