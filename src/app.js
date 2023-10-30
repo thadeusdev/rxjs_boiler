e2adb7d960b6c354c796211b9980f7f1a7f960d4
thadeus <thadeusnyariki94@gmail.com>
2023-10-30 18:29:45 +0300

    Observables from a promise

--- src/app.js
+++ src/app.js
@@ -1,4 +1,47 @@
 import $ from 'jquery';
 import Rx from 'rxjs/Rx';
 
-console.log('RxJS Boiler Running...');
+const myPromise = new Promise((resolve, reject) => {
+  console.log('creating Promise');
+  setTimeout(() => {
+    resolve('Hello from promise');
+  }, 3000);
+});
+
+/*
+myPromise.then(x => {
+    console.log(x);
+})
+*/
+
+/*
+const source$ = Rx.Observable.fromPromise(myPromise);
+
+source$.subscribe(x => console.log(x));
+*/
+
+function getUser(username) {
+  return $.ajax({
+    url: 'https://api.github.com/users/' + username,
+    dataType: 'jsonp'
+  }).promise();
+}
+
+const inputSource$ = Rx.Observable.fromEvent($('#input'), 'keyup');
+
+inputSource$.subscribe((e) => {
+  Rx.Observable.fromPromise(getUser(e.target.value)).subscribe((x) => {
+    $('#name').text(x.data.name);
+    $('#blog').text(x.data.blog);
+    $('#repos').text('Public Repos: ' + x.data.public_repos);
+  });
+});
+
+/*
+Rx.Observable.fromPromise(getUser('thadeusdev')).subscribe((x) => {
+  console.log(x);
+  $('#name').text(x.data.name);
+  $('#blog').text(x.data.blog);
+  $('#repos').text('Public Repos: ' + x.data.public_repos);
+});
+*/
