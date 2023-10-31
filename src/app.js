3701495d4d3378ea3ac67fb7f1ace97e54cfd09a
thadeus <thadeusnyariki94@gmail.com>
2023-10-31 08:55:58 +0300

    interval, timer and Range

--- src/app.js
+++ src/app.js
@@ -1,4 +1,51 @@
 import $ from 'jquery';
 import Rx from 'rxjs/Rx';
 
-console.log('RxJS Boiler Running...');
+/*
+// Interval
+const source$ = Rx.Observable.interval(1000).take(5);
+
+source$.subscribe(
+    x => {
+        console.log(x);
+    },
+    err => {
+        console.log(err);
+    },
+    complete => {
+        console.log('completed');
+    }
+);
+*/
+
+/*
+// Timer
+const source$ = Rx.Observable.timer(5000, 2000).take(5);
+
+source$.subscribe(
+    x => {
+        console.log(x);
+    },
+    err => {
+        console.log(err);
+    },
+    complete => {
+        console.log('completed');
+    }
+)
+*/
+
+// Range
+const source$ = Rx.Observable.range(25, 100);
+
+source$.subscribe(
+    x => {
+        console.log(x);
+    },
+    err => {
+        console.log(err);
+    },
+    complete => {
+        console.log('completed');
+    }
+)
